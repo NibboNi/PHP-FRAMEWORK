38c6c3cd8dbe63034aa7281ec59c7790bf7414a1
Nico G <131214845+NibboNi@users.noreply.github.com>
2026-07-21 17:34:08 -0600

    extract database connection credentials

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
 spl_autoload_register(function (string $className) {
@@ -8,14 +10,19 @@ spl_autoload_register(function (string $className) {
 
 $router = new Framework\Router;
 
-$router->addRoute("/{title}/{id:\d+}/{page}", ["controller" => "products", "action" => "showPage"]);
-$router->addRoute("/admon", ["controller" => "users", "action" => "index", "namespace" => "Admin"]);
 $router->addRoute("/", ["controller" => "home", "action" => "index"]);
 $router->addRoute("/products", ["controller" => "products", "action" => "index"]);
 $router->addRoute("/products/show", ["controller" => "products", "action" => "show"]);
 $router->addRoute("/product/{slug:[\w-]+}", ["controller" => "products", "action" => "show"]);
 $router->addRoute("/{controller}/{id:\d+}/{action}");
 $router->addRoute("/{controller}/{action}");
+// $router->addRoute("/admon", ["controller" => "users", "action" => "index", "namespace" => "Admin"]);
+// $router->addRoute("/{title}/{id:\d+}/{page}", ["controller" => "products", "action" => "showPage"]);
 
-$dispatcher = new Framework\Dispatcher($router);
+$container = new Framework\Container;
+$container->set(App\Database::class, function () {
+  return new App\Database("", "", "", "");
+});
+
+$dispatcher = new Framework\Dispatcher($router, $container);
 $dispatcher->handle($path);
--- src/App/Database.php
+++ src/App/Database.php
@@ -1,17 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 use PDO;
 
 class Database
 {
+  public function __construct(private string $host, private string $name, private string $user, private string $password) {}
 
   public function getConnection(): PDO
   {
-    $dsn = "mysql:host=;dbname=;port:;charset=utf8";
+    $dsn = "mysql:host={$this->host};dbname={$this->name};port:3306;charset=utf8";
 
-    return new PDO($dsn, "", "", [
+    return new PDO($dsn, $this->user, $this->password, [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
   }
